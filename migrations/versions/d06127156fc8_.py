be303aa3eb05b0c719c61565191374872fe0508b
alejandra fusil <alejandrafusil@gmail.com>
2022-04-07 17:06:05 +0000

    reset migrations

--- migrations/versions/d06127156fc8_.py
+++ migrations/versions/d06127156fc8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 77168f79a170
+Revision ID: d06127156fc8
 Revises: 
-Create Date: 2022-04-07 01:24:31.309104
+Create Date: 2022-04-07 17:05:32.309827
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '77168f79a170'
+revision = 'd06127156fc8'
 down_revision = None
 branch_labels = None
 depends_on = None
